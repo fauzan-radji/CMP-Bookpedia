1bc6b31936e6859b9cdde11f0162b885f862cbd4
Fauzan Radji <72055502+fauzan-radji@users.noreply.github.com>
2025-04-20 00:31:02 +0800

    ui: added searchbar

---
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
@@ -0,0 +1,18 @@
+package com.plcoding.bookpedia
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
+
+@Preview(backgroundColor = 0xFFFFFFFF, showBackground = true)
+@Composable
+fun BookSearchBarPreview() {
+    BookSearchBar(
+        searchQuery = "Kotlin",
+        onSearchQueryChange = {},
+        onImeSearch = {},
+        modifier = Modifier.fillMaxWidth()
+    )
+}
